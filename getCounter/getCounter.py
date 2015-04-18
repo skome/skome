cf4b9255b8a1b0d8f2ec1ce4e3f7a4551d19f88e
Sam Kome <samk91711@yahoo.com>
2015-04-18 13:13:18 -0700

    moved constants

--- getCounter/getCounter.py
+++ getCounter/getCounter.py
@@ -1,8 +1,19 @@
 import pycounter.sushi
 import datetime
+weekdays = {1:"Mon",2:"Tue",3:"Wed",4:"Thu",5:"Fri",6:"Sat",7:"Sun"}
+START = datetime.date(2015,1,1)
+END = datetime.date(2015,4,15)
+RID = "b29daa24-cc02-4760-8694-3084f90445a7"
+CR = "X054213"
+REP = "JR1"
+REL = 4
+COUNTERREP = pycounter.sushi.get_report(wsdl_url="http://sushi4.scholarlyiq.com/SushiService.svc", 
+start_date=START, 
+end_date=END, 
+requestor_id=RID,
+customer_reference=CR,
+report=REP,release=REL)
 
-report = pycounter.sushi.get_report(wsdl_url="http://sushi4.scholarlyiq.com/SushiService.svc", start_date=datetime.date(2015,1,1), enddate=datetime.date(2015,4,15), requestor_id="b29daa24-cc02-4760-8694-3084f90445a7",customer_reference="X054213",report="JR1",release=4)
-
-for row in report:
+for row in COUNTERREP:
     for metric in row:
-        print row.title, row.eissn, row.publisher, metric[0],metric[2]
+        print row.title, row.eissn, row.publisher, metric[0].year, metric[0].month,metric[0].day,weekdays[metric[0].weekday()],metric[2]
